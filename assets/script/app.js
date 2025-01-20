5cbc530ab8de15af90ea291b64644c8f1ad2d3b4
ROHIT <164467705+rohit02q@users.noreply.github.com>
2025-01-20 13:59:31 +0530

    Add files via upload

---
+++ assets/script/app.js
@@ -0,0 +1,121 @@
+const user_choice = document.getElementById('user-choice');
+const Rock_box = document.getElementById('Rock');
+const Paper_box = document.getElementById('Paper');
+const Scissors_box = document.getElementById('Scissors');
+const winmsgbox = document.getElementById('status');
+const winmsg = document.getElementById('winmsg');
+const userScore = document.getElementById('user-score');
+const comScore = document.getElementById('com-score');
+const comimg = document.getElementById('com-choice');
+
+let user_point = 0;
+let com_point = 0;
+let isStart = false;
+
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+function gen_com_choice() {
+  comimg.src = `assets/img/rock2.png`;
+
+  const y = choices[Math.floor(Math.random() * 3)];
+  setTimeout(() => {
+
+    comimg.src = `assets/img/${y.toLowerCase()}2.png`;
+
+  }, 1500)
+  return y;
+
+
+}
+
+function shake() {
+  user_choice.classList.add('user-hand');
+  comimg.classList.add('com-hand');
+  setTimeout(() => {
+    user_choice.classList.remove('user-hand');
+    comimg.classList.remove('com-hand');
+  }, 1500)
+}
+
+function checkwin(x, y) {
+  shake();
+  setTimeout(() => {
+
+
+    if (x === y) {
+      winmsg.innerText = 'Match Draw!';
+      winmsg.style.color = 'blueviolet';
+    } else if (
+      (x === "Rock" && y === "Scissors") ||
+      (x === "Paper" && y === "Rock") ||
+      (x === "Scissors" && y === "Paper")
+    ) {
+      winmsg.innerText = 'You won!!';
+      winmsg.style.color = 'green';
+      user_point++;
+      userScore.innerText = user_point;
+    } else {
+      winmsg.innerText = 'You lose!!';
+      winmsg.style.color = 'red';
+      com_point++;
+      comScore.innerText = com_point;
+    }
+    isStart = false;
+
+
+  }, 1500);
+}
+
+if (!isStart) {
+  Rock_box.addEventListener('click', () => {
+    user_choice.src = `assets/img/rock2.png`;
+    winmsg.style.color = 'blueviolet';
+    winmsg.innerText = 'Wait';
+    if (isStart) return; 
+    isStart = true;
+
+    const x = "Rock";
+    const y = gen_com_choice();
+    checkwin(x, y);
+    setTimeout(() => {
+
+      user_choice.src = `assets/img/rock2.png`;
+
+    }, 1500)
+
+  });
+
+  Paper_box.addEventListener('click', () => {
+    user_choice.src = `assets/img/rock2.png`;
+    winmsg.style.color = 'blueviolet';
+    winmsg.innerText = 'Wait';
+    if (isStart) return;
+    isStart = true;
+
+    const x = "Paper";
+    const y = gen_com_choice();
+    checkwin(x, y);
+    setTimeout(() => {
+
+      user_choice.src = `assets/img/paper2.png`;
+
+    }, 1500)
+  });
+
+  Scissors_box.addEventListener('click', () => {
+    user_choice.src = `assets/img/rock2.png`;
+    winmsg.style.color = 'blueviolet';
+    winmsg.innerText = 'Wait';
+    if (isStart) return;
+    isStart = true;
+
+    const x = "Scissors";
+    const y = gen_com_choice();
+    checkwin(x, y);
+    setTimeout(() => {
+
+      user_choice.src = `assets/img/scissors2.png`;
+
+    }, 1500)
+  });
+}
